a4729bcd44c05911e5b35b635dc0cf86e72bbb3f
bbouslog <36169367+bbouslog@users.noreply.github.com>
2022-10-14 10:09:19 -0500

    Add Basic Layout

---
+++ recipe_database/recipe_database.py
@@ -0,0 +1,55 @@
+import json
+import time
+import recipe
+from tqdm import tqdm
+import hello_fresh as hf
+from multiprocessing import Pool, Lock
+
+__recipe_database_filename = 'recipe_database.json'
+
+def __verbose_print(to_print, verbose):
+    if verbose:
+        print(to_print)
+
+def __parse_all_recipes_helper_hf(url):
+    to_return = None
+    try:
+        to_return = hf.get_recipe(url)
+    except recipe.RecipeParseException:
+        pass
+    return to_return
+
+def __parse_all_recipes(verbose=True):
+    all_recipes = []
+    all_urls_hf = hf.find_recipe_urls(verbose=verbose)
+    __verbose_print('Parsing Hello Fresh Recipes', verbose)
+
+    start_time = time.time()
+    pool = Pool(8)
+    return_recipes = pool.map(__parse_all_recipes_helper_hf, all_urls_hf)
+    pool.close()
+    end_time = time.time()
+    return_recipes = [recipe for recipe in return_recipes if recipe is not None]
+    __verbose_print('Parsed ' + str(len(return_recipes)) + ' recipes from Hello Fresh', verbose)
+    __verbose_print('Elapsed Time: ' + str(end_time-start_time) + '\tTime per Iteration: ' + str((end_time-start_time)/len(all_urls_hf)), verbose)
+    all_recipes.extend(return_recipes)
+
+    return all_recipes
+
+def __save_all_recipes(verbose=True):
+    all_recipes = __parse_all_recipes(verbose=verbose)
+    all_recipes_dict = {'all_recipes': [recipe_obj.get_recipe_dict() for recipe_obj in all_recipes]}
+
+    with open(__recipe_database_filename, 'w') as output_file:
+        json.dump(all_recipes_dict, output_file)
+
+def read_all_recipes():
+    all_recipes_dict = []
+    with open(__recipe_database_filename, 'r') as input_file:
+        all_recipes_dict = json.load(input_file)
+
+    all_recipes = [recipe.Recipe(recipe_dict=recipe_dict) for recipe_dict in all_recipes_dict['all_recipes']]
+    return all_recipes
+
+if __name__ == "__main__":
+    __save_all_recipes(verbose=True)
---
+++ script.js
@@ -0,0 +1,58 @@
+
+recipe_database_filename = 'recipe_database/recipe_database.json'
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+function readAllRecipes(){
+    recipe_dict_list = readTextFile(recipe_database_filename, function(text){
+        var data = JSON.parse(text);
+        console.log(data[0]);
+    });
+}
+
+function getTotalRecipes(){
+    var total_serv = document.getElementById("total-serv").value;
+    var serv_per_recipe = document.getElementById("serv-per-recipe").value;
+    document.getElementById('total-recipe').innerHTML = Math.ceil(total_serv / serv_per_recipe).toString()
+}
+
+function getTextAreaList(ta_id){
+    var ta_str = document.getElementById(ta_id).value;
+    ta_str = ta_str.replaceAll(", ", ",")
+    var ta_list = ta_str.split(",")
+    return ta_list
+}
+
+readTextFile("/Users/Documents/workspace/test.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
+
+function pickRecipes(){
+    var req_itms = getTextAreaList("req-itms")
+    var unwant_itms = getTextAreaList("unwant-itms")
+    var total_serv = document.getElementById("total-serv").value;
+    var serv_per_recipe = document.getElementById("serv-per-recipe").value;
+
+    return_text = "<div>" + total_serv.toString() + "</div>"
+    return_text += "<div>" + serv_per_recipe.toString() + "</div><div>"
+    for (let i = 0; i < req_itms.length; i++) {
+        return_text += req_itms[i] + "|^";
+    }
+    return_text = return_text.substring(0, return_text.length - 2) + "</div><div>"
+    for (let i = 0; i < unwant_itms.length; i++) {
+        return_text += unwant_itms[i] + "|^";
+    }
+    return_text = return_text.substring(0, return_text.length - 2) + "</div>"
+    document.getElementById('test').innerHTML = return_text
+}
