d2bc74f8772843d77300f7c904ea1021063887ad
bbouslog <36169367+bbouslog@users.noreply.github.com>
2022-10-14 14:20:18 -0500

    Adding recipe checking

--- scripts/generate.js
+++ scripts/generate.js
@@ -19,7 +19,42 @@ function getTextAreaList(ta_id){
     var ta_str = document.getElementById(ta_id).value;
     ta_str = ta_str.replaceAll(", ", ",")
     var ta_list = ta_str.split(",")
+    for (let i = 0; i < ta_list.length; i++) {
+        ta_list[i] = ta_list[i].toLowerCase()
+    }
     return ta_list
+}
+
+function isValidRecipe(recipe_idx, unwant_itms){
+    var recipe_name = recipe_database.all_recipes[recipe_idx].name
+    var recipe_df = recipe_database.all_recipes[recipe_idx].recipe
+
+    if (recipe_df.length == 0) {
+        return false
+    }
+    else if (unwant_itms.length != 0) {
+        recipe_name_words = recipe_name.toLowerCase().split(" ")
+        for (let i = 0; i < recipe_name_words.length; i++) {
+            if (unwant_itms.includes(recipe_name_words[i])) {
+                return false
+            }
+        }
+
+        for (let i = 0; i < recipe_df.length; i++) {
+            var recipe_item = recipe_df[i].item.toLowerCase()
+            if (unwant_itms.includes(recipe_item)) {
+                return false
+            }
+
+            item_words = recipe_item.split(" ")
+            for (let i = 0; i < item_words.length; i++) {
+                if (unwant_itms.includes(item_words[i])) {
+                    return false
+                }
+            }
+        }
+    }
+    return true
 }
 
 function pickRecipes(){
@@ -27,6 +62,8 @@ function pickRecipes(){
     var unwant_itms = getTextAreaList("unwant-itms")
     var total_serv = document.getElementById("total-serv").value;
     var serv_per_recipe = document.getElementById("serv-per-recipe").value;
+
+    valid_recipe = isValidRecipe(0, unwant_itms)
 
     var return_text = "<div>" + total_serv.toString() + "</div>"
     return_text += "<div>" + serv_per_recipe.toString() + "</div><div>"
@@ -40,6 +77,7 @@ function pickRecipes(){
     return_text = return_text.substring(0, return_text.length - 2) + "</div>"
     return_text += "<div>" + recipe_database.all_recipes[0].name + "</div>"
     return_text += "<div>" + recipe_database.all_recipes[0].url + "</div>"
+    return_text += "<div>" + valid_recipe.toString() + "</div>"
     console.log(return_text)
     document.getElementById('test').innerHTML = return_text
 }
